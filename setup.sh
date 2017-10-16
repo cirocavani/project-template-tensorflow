83d9170c53fafccd8061522df4530be4fe117f75
Ciro Cavani <ciro.cavani@gmail.com>
2017-10-16 20:40:05 -0200

    Miniconda 4.3.21 -> 4.3.27.1

--- setup.sh
+++ setup.sh
@@ -9,14 +9,15 @@ mkdir -p .cache
 
 echo "Setup..."
 
-if [ ! -f .cache/Miniconda3-4.3.21-Linux-x86_64.sh ]; then
+if [ ! -f .cache/Miniconda3-4.3.27.1-Linux-x86_64.sh ]; then
     curl -k -L \
-        https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.sh \
-        -o .cache/Miniconda3-4.3.21-Linux-x86_64.sh
+        https://repo.continuum.io/miniconda/Miniconda3-4.3.27.1-Linux-x86_64.sh \
+        -o .cache/Miniconda3-4.3.27.1-Linux-x86_64.sh
 fi
 rm -rf $CONDA_HOME
-bash .cache/Miniconda3-4.3.21-Linux-x86_64.sh -b -f -p $CONDA_HOME
+bash .cache/Miniconda3-4.3.27.1-Linux-x86_64.sh -b -f -p $CONDA_HOME
 
+$CONDA_HOME/bin/conda update -y conda
 $CONDA_HOME/bin/conda update -y --all
 $CONDA_HOME/bin/conda install -y jupyter
 $CONDA_HOME/bin/pip install tensorflow
