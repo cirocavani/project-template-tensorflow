6f49de6adf7da4c3c10ecfaa6510d8befaa02145
Ciro Cavani <ciro.cavani@gmail.com>
2019-06-20 19:37:24 -0300

    Update TensorFlow 2.0b1, Python 3.7

--- conf/env.sh
+++ conf/env.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-set -eu
-
 export PROJECT_HOME=$(pwd)
-
-export CONDA_HOME=$PROJECT_HOME/software/conda
-export JUPYTER_DATA_DIR=$PROJECT_HOME/software/jupyter_data
+export CONDA_INSTDIR=$PROJECT_HOME/software/conda
+export JUPYTER_DATA_DIR=$PROJECT_HOME/software/jupyter-data
+export PATH=$CONDA_INSTDIR/bin:$PATH
--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
 
 cd $(dirname "$0")/..
@@ -9,22 +9,21 @@ mkdir -p .cache
 
 echo "Installing..."
 
-CONDA_PKG=Miniconda3-4.5.1-Linux-x86_64.sh
+CONDA_PKG=Miniconda3-4.6.14-Linux-x86_64.sh
+CONDA_URL=https://repo.anaconda.com/miniconda/$CONDA_PKG
 
 if [ ! -f .cache/$CONDA_PKG ]; then
-    curl -k -L \
-        https://repo.continuum.io/miniconda/$CONDA_PKG \
-        -o .cache/$CONDA_PKG
+    curl -k -L -o .cache/$CONDA_PKG $CONDA_URL
 fi
 
-rm -rf $CONDA_HOME
+rm -rf $CONDA_INSTDIR
 rm -rf $JUPYTER_DATA_DIR
 mkdir -p $JUPYTER_DATA_DIR
 
-bash .cache/$CONDA_PKG -b -f -p $CONDA_HOME
+bash .cache/$CONDA_PKG -b -f -p $CONDA_INSTDIR
 
-$CONDA_HOME/bin/conda update -q -y conda
-$CONDA_HOME/bin/conda update -q -y --all
-$CONDA_HOME/bin/conda env update -q -n base -f setup/environment.yml
+$CONDA_INSTDIR/bin/conda update -q -y conda
+$CONDA_INSTDIR/bin/conda update -q -y --all
+$CONDA_INSTDIR/bin/conda env update -q -n base -f setup/environment.yml
 
 echo "Install done!"
